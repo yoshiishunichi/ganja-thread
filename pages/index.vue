f33ad144aecc54bb501e22853da3c9107925c4fc
吉井駿一 <syun_syun01@icloud.com>
2021-03-29 10:24:06 +0900

    スタイル仮置き

--- pages/index.vue
+++ pages/index.vue
@@ -2,7 +2,11 @@
   <div class="container">
     <h1>ganja_tuberについて語るスレ</h1>
     <div class="threads-container">
-      <div v-for="(thread, index) in $store.getters.getThreads" :key="index">
+      <div
+        v-for="(thread, index) in $store.getters.getThreads"
+        :key="index"
+        class="thread"
+      >
         <p>No. {{ index + 1 }}</p>
         <p>{{ thread.postedAt.toDate() }}</p>
         <h3>{{ thread.name }}さん</h3>
@@ -20,14 +24,29 @@ export default {
 }
 </script>
 
-<style>
+<style scoped>
 .container {
   margin: 0 auto;
   min-height: 100vh;
-  display: flex;
   justify-content: center;
   align-items: center;
   text-align: center;
-  flex-direction: column;
+}
+
+h1 {
+  font-size: 30px;
+  margin-top: 24px;
+  margin-bottom: 24px;
+  color: white;
+}
+
+.thread {
+  background: white;
+  margin-bottom: 12px;
+  margin-left: auto;
+  margin-right: auto;
+  width: 95%;
+  max-width: 1000px;
+  border-radius: 15px;
 }
 </style>
