3c4e8f62dba388ec250196854b47a608b17cfb34
吉井駿一 <syun_syun01@icloud.com>
2021-03-29 09:27:50 +0900

    store側作成

---
+++ store/index.js
@@ -0,0 +1,57 @@
+import firebase from '~/plugins/firebase'
+
+const db = firebase.firestore()
+const threadsRef = db.collection('threads')
+
+export const state = () => ({
+  postedAt: new Date(),
+  name: '',
+  text: '',
+  threads: [],
+})
+
+export const mutations = {
+  setName(state, name) {
+    state.name = name
+  },
+  setText(state, text) {
+    state.text = text
+  },
+  setTime(state) {
+    state.postedAt = Date.now()
+  },
+  addThread(state, thread) {
+    state.threads.push(thread)
+  },
+}
+
+export const actions = {
+  fetchNewStatus({ commit }) {
+    threadsRef
+      .get()
+      .then((res) => {
+        res.forEach((doc) => {
+          console.log('success :' + `${doc.id} => ${doc.data()}`)
+          commit.add('addThread', doc.data())
+        })
+      })
+      .catch((err) => {
+        console.log('error :' + err)
+      })
+  },
+}
+
+export const getters = {
+  getName(state) {
+    return state.name
+  },
+  getText(state) {
+    return state.text
+  },
+  getTime(state) {
+    return state.postedAt
+  },
+  getThreads(state) {
+    return state.threads
+  },
+}
